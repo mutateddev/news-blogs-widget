5141af94affae845fea8b8ac1d82ddefd9f5b282
mutateddev <mutateddev@gmail.com>
2026-06-07 13:05:22 +0330

    feat: add calendar widget ui

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,7 +1,63 @@
-import "./Calendar.css";
+import { ChevronLeft, ChevronRight } from '@boxicons/react';
+import './Calendar.css';
 
 const Calendar = () => {
-  return <div className="calendar">calendar</div>;
+  return (
+    <div className='calendar'>
+      <div className='navigate-date'>
+        <h2 className='month'>July,</h2>
+        <h2 className='year'>2026</h2>
+        <div className='buttons'>
+          <ChevronLeft className='icon' />
+          <ChevronRight className='icon' />
+        </div>
+      </div>
+
+      <div className='weekdays'>
+        <span>Sun</span>
+        <span>Mon</span>
+        <span>Tue</span>
+        <span>Wed</span>
+        <span>Thu</span>
+        <span>Fri</span>
+        <span>Sat</span>
+      </div>
+
+      <div className='days'>
+        <span className='current-day'>1</span>
+        <span>2</span>
+        <span>3</span>
+        <span>4</span>
+        <span>5</span>
+        <span>6</span>
+        <span>7</span>
+        <span>8</span>
+        <span>9</span>
+        <span>10</span>
+        <span>11</span>
+        <span>12</span>
+        <span>13</span>
+        <span>14</span>
+        <span>15</span>
+        <span>16</span>
+        <span>17</span>
+        <span>18</span>
+        <span>19</span>
+        <span>20</span>
+        <span>21</span>
+        <span>22</span>
+        <span>23</span>
+        <span>24</span>
+        <span>25</span>
+        <span>26</span>
+        <span>27</span>
+        <span>28</span>
+        <span>29</span>
+        <span>30</span>
+        <span>31</span>
+      </div>
+    </div>
+  );
 };
 
 export default Calendar;
