e5fe2b0d38e699b53c3a874435309fafc2a8bec9
mutateddev <mutateddev@gmail.com>
2026-06-07 14:30:13 +0330

    feat(calendar): add calendar functionality

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,60 +1,76 @@
 import { ChevronLeft, ChevronRight } from '@boxicons/react';
 import './Calendar.css';
+import { useState } from 'react';
 
 const Calendar = () => {
+  const daysOfWeak = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  const monthsOfYear = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  const currentDate = new Date();
+  const [currentMonth, setCurrentMonth] = useState(currentDate.getMonth());
+  const [currentYear, setCurrentYear] = useState(currentDate.getFullYear());
+  const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+  const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
+
+  const prevMonth = () => {
+    setCurrentMonth(prevMonth => (prevMonth === 0 ? 11 : prevMonth - 1));
+    setCurrentYear(prevYear => (currentMonth === 0 ? prevYear - 1 : prevYear));
+  };
+  const nextMonth = () => {
+    setCurrentMonth(prevMonth => (prevMonth === 11 ? 0 : prevMonth + 1));
+    setCurrentYear(prevYear => (currentMonth === 11 ? prevYear + 1 : prevYear));
+  };
+
   return (
     <div className='calendar'>
       <div className='navigate-date'>
-        <h2 className='month'>July,</h2>
-        <h2 className='year'>2026</h2>
+        <h2 className='month'>{monthsOfYear[currentMonth]},</h2>
+        <h2 className='year'>{currentYear}</h2>
         <div className='buttons'>
-          <ChevronLeft className='icon' />
-          <ChevronRight className='icon' />
+          <ChevronLeft className='icon' size='sm' onClick={prevMonth} />
+          <ChevronRight className='icon' size='sm' onClick={nextMonth} />
         </div>
       </div>
 
       <div className='weekdays'>
-        <span>Sun</span>
-        <span>Mon</span>
-        <span>Tue</span>
-        <span>Wed</span>
-        <span>Thu</span>
-        <span>Fri</span>
-        <span>Sat</span>
+        {daysOfWeak.map(day => (
+          <span key={day}>{day}</span>
+        ))}
       </div>
 
       <div className='days'>
-        <span className='current-day'>1</span>
-        <span>2</span>
-        <span>3</span>
-        <span>4</span>
-        <span>5</span>
-        <span>6</span>
-        <span>7</span>
-        <span>8</span>
-        <span>9</span>
-        <span>10</span>
-        <span>11</span>
-        <span>12</span>
-        <span>13</span>
-        <span>14</span>
-        <span>15</span>
-        <span>16</span>
-        <span>17</span>
-        <span>18</span>
-        <span>19</span>
-        <span>20</span>
-        <span>21</span>
-        <span>22</span>
-        <span>23</span>
-        <span>24</span>
-        <span>25</span>
-        <span>26</span>
-        <span>27</span>
-        <span>28</span>
-        <span>29</span>
-        <span>30</span>
-        <span>31</span>
+        {/* empty day of previous day */}
+        {[...Array(firstDayOfMonth).keys()].map((d, i) => {
+          return <span key={`empty-${i}`}></span>;
+        })}
+
+        {/* days of month */}
+        {[...Array(daysInMonth).keys()].map(day => (
+          <span
+            key={day + 1}
+            className={
+              day + 1 === currentDate.getDate() &&
+              currentMonth === currentDate.getMonth() &&
+              currentYear === currentDate.getFullYear()
+                ? 'current-day'
+                : ''
+            }
+          >
+            {day + 1}
+          </span>
+        ))}
       </div>
     </div>
   );
