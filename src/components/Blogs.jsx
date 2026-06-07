e03258883f9e0f1a1b1a58dac3b7a8b7e80afda9
mutateddev <mutateddev@gmail.com>
2026-06-07 16:47:41 +0330

    feat(blogs):create add blog form and styling

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,4 +1,9 @@
-import { ChevronRight } from '@boxicons/react';
+import {
+  ArrowFromBottom,
+  ArrowFromBottomStroke,
+  ArrowUpDown,
+  ChevronRight,
+} from '@boxicons/react';
 import './Blogs.css';
 import userImage from '../assets/images/user.jpg';
 
@@ -10,7 +15,34 @@ const Blogs = () => {
       </div>
 
       <div className='blogs-right'>
-        <button className='post-btn'>Create New Post</button>
+        {/* <button className='post-btn'>Create New Post</button> */}
+        <div className='blogs-right-form'>
+          <h1>New Post</h1>
+          <form>
+            <div className='img-upload'>
+              <label htmlFor='file-upload' className='file-upload'>
+                <ArrowFromBottom
+                  className='upload-icon'
+                  fill='#b88efc'
+                  size='md'
+                />
+                Upload Image
+              </label>
+              <input type='file' id='file-upload' />
+            </div>
+            <input
+              type='text'
+              placeholder='Add Title (Max 60 Character)'
+              max={60}
+              className='title-input'
+            />
+
+            <textarea className='text-input' placeholder='Add Text' />
+            <button type='submit' className='submit-btn'>
+              Submit Button
+            </button>
+          </form>
+        </div>
         <button className='blogs-close-btn'>
           Back <ChevronRight className='back-btn' size='md' />
         </button>
